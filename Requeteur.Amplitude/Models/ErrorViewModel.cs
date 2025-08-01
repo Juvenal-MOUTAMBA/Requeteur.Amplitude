1684fae0d4ce6cd3b3a4c5e817a44b21b5074b13
juvenalmoutamba <juvenal.moutamba@gmail.com>
2025-08-01 10:20:05 +0100

    Démarrage du projet

---
+++ Requeteur.Amplitude/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Requeteur.Amplitude.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
