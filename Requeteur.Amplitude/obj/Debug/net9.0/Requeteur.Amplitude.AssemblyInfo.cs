2aac00090d9a02f4072ccc1a5c9fd01eacd619c8
juvenalmoutamba <juvenal.moutamba@gmail.com>
2025-08-21 13:19:05 +0100

    Configuration connexion

--- Requeteur.Amplitude/obj/Debug/net9.0/Requeteur.Amplitude.AssemblyInfo.cs
+++ Requeteur.Amplitude/obj/Debug/net9.0/Requeteur.Amplitude.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Requeteur.Amplitude")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4d7ec2e3e059a64792203d81ca969dfff3635a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fdd855d2b05edf1af15137f4fe9ec0b7baa65e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Requeteur.Amplitude")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Requeteur.Amplitude")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
